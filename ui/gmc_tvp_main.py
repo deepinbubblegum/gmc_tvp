a9e8bfa16bd4ac4e9a875850784397a5e679b4c5
deepinbubblegum <5911110222@mutacth.com>
2021-09-23 02:11:41 +0700

    update

--- ui/gmc_tvp_main.py
+++ ui/gmc_tvp_main.py
@@ -5,4 +5,13 @@ class UiMain(QtWidgets.QMainWindow):
     def __init__(self):
         super(UiMain, self).__init__()
         uic.loadUi('./ui/resource/gmc_tvp_main.ui', self)
+        self.load_component()
         self.show()
+        
+    def load_component(self):
+        # btn component
+        self.btn_setting = self.findChild(QtWidgets.QPushButton, "btnSetting")
+        self.btn_setting.clicked.connect(self.invoke_form_settings)
+        
+    def invoke_form_settings(self):
+        print("hello")
